a6145b5024e586b16ee10375624d0d946f1de818
dimor22 <Green@p1e>
2018-05-18 17:59:12 -0700

    all layout finished

--- js/main.js
+++ js/main.js
@@ -1,9 +1,44 @@
-$('.record-edit-btn').click(function () {
-    $('.record-container').show();
-    var recordContainer = $(this).parent().parent().find('.record-container');
-    if (recordContainer.is(":visible")) {
-        recordContainer.hide();
-    } else {
-        recordContainer.show();
-    }
+var wrapperIdName = 'records2';
+
+$('#'+ wrapperIdName + ' .record-edit-btn').click(function () {
+
+    $('#'+ wrapperIdName + ' .record-edit-btn').show();
+
+    $(this).hide();
+
+    $('#'+ wrapperIdName + ' .record-remove-btn').hide();
+
+    $(this).siblings('.record-remove-btn').show();
+
+    $('#'+ wrapperIdName + ' .record-add-btn').show();
+
 });
+
+$('#'+ wrapperIdName + ' .record-btn-close').click(function () {
+    var panelId = $(this).closest('.record-details').attr('id');
+
+    $(this).closest('.record-details').siblings('.record-edit-section').find('.record-edit-btn').show();
+
+    $(this).closest('.record-details').siblings('.record-edit-section').find('.record-remove-btn').hide();
+
+    $('#'+ panelId).collapse('hide');
+
+});
+
+$('#'+ wrapperIdName + ' .record-add-btn').click(function () {
+
+    $(this).hide();
+
+    $('#'+ wrapperIdName + ' .record-edit-btn').show();
+
+    $('#'+ wrapperIdName + ' .record-remove-btn').hide();
+
+});
+
+$('#'+ wrapperIdName + ' .new-record-cancel').click(function () {
+    var panelId = $(this).closest('.record-details').attr('id');
+
+    $('.record-add-btn').show();
+
+    $('#'+ panelId).collapse('hide');
+})
